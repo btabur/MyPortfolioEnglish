f8c1a9a34508844f4808ec73b7bbeac1ecbdb72e
btabur <webtasarim.0323@gmail.com>
2023-09-11 17:20:44 +0300

    finished home,about and skills

---
+++ README.md
@@ -0,0 +1,2 @@
+# MyPortfolioEnglish
+- My web site
---
+++ main.js
@@ -0,0 +1,8 @@
+var typed = new Typed('.text', {
+    strings:["Frontend Developer", "Web Developer"],
+    typeSpeed:100,
+    backSpedd:100,
+    backDelay:1000,
+    loop:true
+})
+
